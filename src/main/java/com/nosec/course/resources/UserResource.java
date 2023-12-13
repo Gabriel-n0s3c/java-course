b4fd6a718e2fbb16f6b10753b19d3fcfcb6fb141
Gabriel Marques <gabriel.marques.o.silva@gmail.com>
2023-12-13 17:28:20 -0300

    User insert

--- src/main/java/com/nosec/course/resources/UserResource.java
+++ src/main/java/com/nosec/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.nosec.course.entities.User;
 import com.nosec.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,17 +18,24 @@ public class UserResource {
     UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-         List<User> listUsers = userService.findAll();
+    public ResponseEntity<List<User>> findAll() {
+        List<User> listUsers = userService.findAll();
         return ResponseEntity.ok().body(listUsers);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
 
         User user = userService.findByID(id);
         return ResponseEntity.ok().body(user);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+
+    }
 
 }
